1d81739dcec5f316243fe477736d3a474b7185c1
anubhabb66@gmail.com <anubhabb66@gmail.com>
2023-07-19 11:48:37 +0530

    initial commit

---
+++ Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conAppSpinGame
+{
+    public delegate void SpinEventhandler(int energyLevelChange, int winningProbabilityChange);
+    public class Game
+    {
+        public event SpinEventhandler SpinEvent;
+
+        public string Name { get; set; }
+        public int LuckyNumber { get; set; }
+        public int EnergyLevel { get;  set; }
+        public int WinningProbability { get;  set; }
+
+        public Game(string name, int luckyNumber)
+        {
+            Name = name;
+            LuckyNumber = luckyNumber;
+            EnergyLevel = 1;
+            WinningProbability = 100;
+        }
+        public void Spin()
+        {
+            if (SpinEvent != null)
+            {
+                SpinEvent(1, 10);
+                SpinEvent(2, 20);
+                SpinEvent(-3, -30);
+                SpinEvent(4, 40);
+                SpinEvent(-5, -50);
+                SpinEvent(-1, -60);
+                SpinEvent(1, 70);
+                SpinEvent(-2, -20);
+                SpinEvent(-3, -30);
+                SpinEvent(10, 100);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conAppSpinGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Luckynumber from 1 to 10: ");
+            int luckyNumber = int.Parse(Console.ReadLine());
+            Game game = new Game(name, luckyNumber);
+
+            SpinEventhandler spinDelegate = (energyLevelChange, winningProbabilityChange) =>
+            {
+                game.EnergyLevel += energyLevelChange;
+                game.WinningProbability += winningProbabilityChange;
+            };
+            game.SpinEvent += spinDelegate;
+            game.Spin();
+
+            if (game.EnergyLevel >= 4 && game.WinningProbability > 60)
+            {
+                Console.WriteLine($"Winner: {game.Name}");
+            }
+            else if (game.EnergyLevel >= 1 && game.WinningProbability > 50)
+            {
+                Console.WriteLine($"Runner up: {game.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"Looser: {game.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
